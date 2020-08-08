afd48b71b4f2079a84e3ae15485c88dcc3a449c0
Teddy Perera <pereratdp@gmail.com>
2020-08-08 17:04:00 +1000

    added pagination and filters

--- src/routers/task.js
+++ src/routers/task.js
@@ -2,12 +2,35 @@ const Task = require('../models/task')
 const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+const { query } = require('express')
 
-//Get all tasks
+//GET /task
+//GET /tasks?completed=false
+//GET /tasks?limit=10&skip=20 
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        const tasks = await Task.find({ owner: req.user._id })
-        res.status(200).send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.status(200).send(req.user.tasks)
     } catch (error) {
         res.status(400).send(error)
     }
