9cea082f86fcffea7af25807575bede2017863ea
Teddy Perera <pereratdp@gmail.com>
2020-08-09 19:55:13 +1000

    added env to mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = process.env.DB_URL
-const databaseName = 'task-manager'
+const databaseName = 'task-manager-api'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -9,12 +9,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-
-    db.collection('tasks').deleteOne({
-        description: "Task 1"
-    }).then((result) => {
-        console.log(result)
-    }).catch((error) => {
-        console.log(error)
-    })
 })
--- src/models/user.js
+++ src/models/user.js
@@ -77,7 +77,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewlanguage')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_TOKEN)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
